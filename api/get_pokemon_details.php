de1979f2ea8d93b66e2be2eeeee4c606cdb04a39
USENGROUP\h-hiranishi <h-hiranishi@usen-next.jp>
2023-06-02 15:59:01 +0900

    add getter of pokemon #1

---
+++ api/get_pokemon_details.php
@@ -0,0 +1,21 @@
+<?php
+
+function get_pokemon_details($id) {
+    $url = "https://pokeapi.co/api/v2/pokemon/{$id}/";
+    $response = file_get_contents($url);
+    $data = json_decode($response, true);
+
+    $pokemon = [];
+    $pokemon["name"] = $data["name"];
+    $pokemon["picture"] = $data["sprites"]["front_default"];
+    $pokemon["height"] = $data["height"];
+    $pokemon["weight"] = $data["weight"];
+    $types = [];
+    foreach ( $data["types"] as $types ) {
+        array_push($types, $types["type"]["name"]);
+    }
+    $pokemon["types"] = $types;
+
+    return $pokemon;
+}
+
