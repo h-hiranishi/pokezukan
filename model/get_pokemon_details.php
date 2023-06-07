e17d3f83a4f4a1612d1184cfae2341ffb8129402
USENGROUP\h-hiranishi <h-hiranishi@usen-next.jp>
2023-06-07 11:09:03 +0900

    日本語での表示を実装した #34

--- model/get_pokemon_details.php
+++ model/get_pokemon_details.php
@@ -5,20 +5,42 @@ require_once __DIR__ . "/get_img.php";
 
 function get_pokemon_details($id) {
     $url = "https://pokeapi.co/api/v2/pokemon/{$id}/";
+    $url_sp = "https://pokeapi.co/api/v2/pokemon-species/{$id}";
     $data = get_contents($url);
+    $data_sp = get_contents($url_sp);
 
     $pokemon = [];
-    $pokemon["name"] = $data["name"];
-    // $pokemon["img_url"] = $data["sprites"]["front_default"];
+    $pokemon["name"] = $data_sp["names"][0]["name"];
+    $pokemon["flavor_text"] = get_jp_flavor($data_sp["flavor_text_entries"]);
     $pokemon["img_name"] = get_img($data["sprites"]["front_default"]);
     $pokemon["height"] = $data["height"];
     $pokemon["weight"] = $data["weight"];
-    $types = [];
-    foreach ( $data["types"] as $type ) {
-        array_push($types, $type["type"]["name"]);
-    }
-    $pokemon["types"] = $types;
+    $pokemon["types"] = get_types($data["types"]);
 
     return $pokemon;
+}
+
+
+function get_jp_flavor($flavor_array) {
+    foreach ( $flavor_array as $flavor_object ) {
+        if ( $flavor_object["language"]["name"] == "ja" ) {
+            return $flavor_object["flavor_text"];
+        }
+    }
+}
+
+
+function get_types($type_array) {
+    $types = [];
+    foreach ( $type_array as $type_object ) {
+        $url = $type_object["type"]["url"];
+        $type_json = get_contents($url);
+        foreach ( $type_json["names"] as $type ) {
+            if ( $type["language"]["name"] == "ja" ) {
+                array_push($types, $type["name"]);
+            }
+        }
+    }
+    return $types;
 }
 
