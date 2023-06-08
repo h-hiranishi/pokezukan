739d9cfc4090e5955ebd4121e75eb9bc833a94dd
USENGROUP\h-hiranishi <h-hiranishi@usen-next.jp>
2023-06-08 09:59:29 +0900

    攻撃を追加 #49

--- model/get_pokemon_details.php
+++ model/get_pokemon_details.php
@@ -12,18 +12,45 @@ function get_pokemon_details($id) {
     $pokemon = [];
     $pokemon["id"] = $data["id"];
     $pokemon["name"] = $data_sp["names"][0]["name"];
-    $pokemon["flavor_text"] = get_jp_flavor($data_sp["flavor_text_entries"]);
-    $pokemon["img_name"] = get_img($data["sprites"]["front_default"]);
+    $pokemon["flavor_text"] = get_flavor($data_sp["flavor_text_entries"]);
+    // $pokemon["img_name"] = get_img($data["sprites"]["front_default"]);
+    $pokemon["img_name"] = get_img($data["sprites"]["other"]["official-artwork"]["front_default"]);
     $pokemon["pokemon_color"] = $data_sp["color"]["name"];
     $pokemon["height"] = $data["height"] * 0.1;
     $pokemon["weight"] = $data["weight"] * 0.1;
     $pokemon["types"] = get_types($data["types"]);
+    $move_info = get_move($data["moves"]);
+    $pokemon["move"] = $move_info["name"];
+    $pokemon["move_type"] = $move_info["type"];
+    $pokemon["move_power"] = $move_info["power"];
 
     return $pokemon;
 }
 
+function get_move($moves) {
+    $index = rand(1, count($moves)-1);
+    $url = $moves[$index]["move"]["url"];
+    $json = get_contents($url);
+    $move = [];
+    $file_name = __DIR__ . "/local_data/type_color.json";
+    $file_handler = fopen($file_name, "r");
+    $json_data = fread($file_handler, filesize($file_name));
+    $type_colors = json_decode($json_data, true);
+    $type = $json["type"]["name"];
+    $move["type"] = $type_colors[$type];
+    // $move["type"] = $json["type"]["name"];
+    
+    $move["power"] = $json["power"];
+    foreach ( $json["names"] as $move_info ) {
+        if ( $move_info["language"]["name"] == "ja" ) {
+            $move["name"] = $move_info["name"];
+            return $move;
+        }
+    }
+}
 
-function get_jp_flavor($flavor_array) {
+
+function get_flavor($flavor_array) {
     foreach ( $flavor_array as $flavor_object ) {
         if ( $flavor_object["language"]["name"] == "ja" ) {
             return $flavor_object["flavor_text"];
